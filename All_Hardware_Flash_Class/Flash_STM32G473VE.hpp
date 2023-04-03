3a9251d5b70b91855d1a6d69822b594ad1d8acc8
Jason Teng <jasonyang.ee@pm.me>
2023-04-02 23:15:16 -0700

    Add: more hardware support

---
+++ All_Hardware_Flash_Class/Flash_STM32G473VE.hpp
@@ -0,0 +1,55 @@
+#ifndef ALL_HARDWARE_FLASH_CLASS_FLASH_STM32G491CE_20COPY
+#define ALL_HARDWARE_FLASH_CLASS_FLASH_STM32G491CE_20COPY
+#ifndef ALL_HARDWARE_FLASH_CLASS_FLASH_STM32G431KB_20COPY
+#define ALL_HARDWARE_FLASH_CLASS_FLASH_STM32G431KB_20COPY
+#ifndef APPLICATION_UNUSED_FLASH_STM32G431KB
+#define APPLICATION_UNUSED_FLASH_STM32G431KB
+
+#include "algorithm"
+#include "array"
+#include "iterator"
+#include "main.h"
+#include "queue"
+
+class Flash {
+   public:
+    Flash();
+    virtual ~Flash();
+
+    void Save();
+    void Load();
+
+   private:
+    // Update config_arr_size according to the sizeof(config)/sizeof(uint64_t)
+	// ------------------- MUST EDIT -------------------
+	#define config_arr_size 2
+    union Config_Arr {
+        struct Config {
+            uint32_t led_level;
+            uint32_t led_scale;
+			uint8_t led_state;
+        } config;
+
+        uint64_t config_arr[config_arr_size];
+    };
+
+	// Begin write on last page last row.
+    // 0x08020000 is the next page of last page.
+    // Same as (0x0807FFFF + 1 - 0x8); if using datasheet provided value
+	// ------------------- MUST EDIT -------------------
+    const uint32_t m_address_end{0x08080000 - 0x8};
+    const uint8_t m_page_total{127};
+
+
+	int32_t Write(const uint64_t*, uint8_t);
+    void Read(Flash::Config_Arr*, uint8_t);
+};
+
+
+#endif    /* APPLICATION_UNUSED_FLASH_STM32G431KB */
+
+
+#endif    /* ALL_HARDWARE_FLASH_CLASS_FLASH_STM32G431KB_20COPY */
+
+
+#endif    /* ALL_HARDWARE_FLASH_CLASS_FLASH_STM32G491CE_20COPY */
